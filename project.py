a527d9e6ed9bdafd5180c865bd5c8f8e1be6f880
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2016-06-26 13:57:08 +0300

    Fix error when no projects opened

--- project.py
+++ project.py
@@ -5,22 +5,24 @@ from codecs import open
 
 class Project:
     PROJECT_FILE_NAME = "package.json"
+    PROJECT_KEY = "nativescript"
 
     @staticmethod
     def get_project_dir(project_path):
-        pair = (project_path, True)
-        while bool(pair[1]):
-            if Project._has_valid_project_file(pair[0]):
-                return pair[0]
-            else:
-                pair = path.split(pair[0])
+        if path.isdir(project_path):
+            pair = (project_path, True)
+            while bool(pair[1]):
+                if Project._has_valid_project_file(pair[0]):
+                    return pair[0]
+                else:
+                    pair = path.split(pair[0])
 
     @staticmethod
     def _has_valid_project_file(project_path):
-        try:
-            json_data = open(path.join(project_path,
-                                       Project.PROJECT_FILE_NAME),
-                             "r", "utf-8")
-            return "nativescript" in load(json_data)
-        except UnicodeError:
-            return False
+        file_name = path.join(project_path, Project.PROJECT_FILE_NAME)
+        if path.isfile(file_name):
+            try:
+                json_data = open(file_name, "r", "utf-8")
+                return Project.PROJECT_KEY in load(json_data)
+            except UnicodeError:
+                return False
